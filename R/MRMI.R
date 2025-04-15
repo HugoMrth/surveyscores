ea337161f66159bf2d5e88f22d7d8a4f94e23d05
Hugo Marthinet <h.marthinet@gmail.com>
2025-04-15 10:51:08 +0200

    Update MRMI.R

--- R/MRMI.R
+++ R/MRMI.R
@@ -14,7 +14,7 @@ MRMI <- function(gender = NULL,
                       parkinson = NULL,
                       mult.sclero = NULL,
                       epilespy = NULL,
-                      chornic.resp = NULL,
+                      chronic.resp = NULL,
                       infl.bowel = NULL,
                       rheum.arthr = NULL,
                       HIV = NULL,
@@ -29,7 +29,7 @@ MRMI <- function(gender = NULL,
       is.null(heart.fail) | is.null(periph.vasc) | is.null(diabetes) | is.null(cancer) |
       is.null(cancer.hist) | is.null(schizo) | is.null(depression) | is.null(subst.abuse) |
       is.null(dementia) | is.null(parkinson) | is.null(mult.sclero) | is.null(epilespy) |
-      is.null(chornic.resp) | is.null(infl.bowel) | is.null(rheum.arthr) | is.null(HIV) |
+      is.null(chronic.resp) | is.null(infl.bowel) | is.null(rheum.arthr) | is.null(HIV) |
       is.null(es.renal) | is.null(liver.pancr)) {
     stop("All arguments are required")
   }
@@ -41,7 +41,7 @@ MRMI <- function(gender = NULL,
       all(depression %in% c(0, 1, TRUE, FALSE)) & all(subst.abuse %in% c(0, 1, TRUE, FALSE)) &
       all(dementia %in% c(0, 1, TRUE, FALSE)) & all(parkinson %in% c(0, 1, TRUE, FALSE)) &
       all(mult.sclero %in% c(0, 1, TRUE, FALSE)) & all(epilespy %in% c(0, 1, TRUE, FALSE)) &
-      all(chornic.resp %in% c(0, 1, TRUE, FALSE)) & all(infl.bowel %in% c(0, 1, TRUE, FALSE)) &
+      all(chronic.resp %in% c(0, 1, TRUE, FALSE)) & all(infl.bowel %in% c(0, 1, TRUE, FALSE)) &
       all(rheum.arthr%in% c(0, 1, TRUE, FALSE)) & all(HIV %in% c(0, 1, TRUE, FALSE)) &
       all(es.renal %in% c(0, 1, TRUE, FALSE)) & all(liver.pancr %in% c(0, 1, TRUE, FALSE)))) {
     stop("All arguments accept type must be c(0,1) or c(TRUE, FALSE)")
@@ -51,14 +51,14 @@ MRMI <- function(gender = NULL,
       isch.heart * 0 + cerebro.vasc * 1 + heart.fail * 1 + periph.vasc * 1 +
         diabetes * 1 + cancer * 3 + cancer.hist * 0 + schizo * 1 + depression * 1 +
           subst.abuse * 2 + dementia * 2 + parkinson * 1 + mult.sclero * 2 + epilespy * 1 +
-            chornic.resp * 1 +  infl.bowel * 0 + rheum.arthr * 1 + HIV * 0 + es.renal * 2 +
+            chronic.resp * 1 +  infl.bowel * 0 + rheum.arthr * 1 + HIV * 0 + es.renal * 2 +
               liver.pancr * 2
 
  ind_ermi <- ifelse(gender == "M", 0, 0) + floor((age - 65)/5) +
       isch.heart * 2 + cerebro.vasc * 3 + heart.fail * 3 + periph.vasc * 3 +
       diabetes * 4 + cancer * 7 + cancer.hist * 2 + schizo * 6 + depression * 5 +
       subst.abuse * 5 + dementia * 2 + parkinson * 5 + mult.sclero * 9 + epilespy * 3 +
-      chornic.resp * 3 +  infl.bowel * 0 + rheum.arthr * 4 + HIV * 10 + es.renal * 16 +
+      chronic.resp * 3 +  infl.bowel * 0 + rheum.arthr * 4 + HIV * 10 + es.renal * 16 +
       liver.pancr * 5
 
 

c69e3d3deacf91ebbec40af388ecbad0500014bf
Hugo Marthinet <h.marthinet@gmail.com>
2025-04-15 10:43:16 +0200

    Update MRMI.R

--- R/MRMI.R
+++ R/MRMI.R
@@ -1,78 +1,69 @@
-MRMI <- function(x = NULL,
-                 genre
-                     age,
-                     isch_heart,
-                     cerebro,
-                     heart_fail,
-                     periph_vasc,
-                     diabetes,
-                     cancer,
-                     cancer_hist,
-                     schiz,
-                     depression
-                     substance_abuse,
-                     dementia,
-                     parkinson,
-                     sclerosis,
-                     epilepsy,
-                     chronic_resp,
-                     infl_bowel,
-                     arthritis,
-                     AIDS,
-                     end_renal,
-                     liver_pancreas
-) {
-  if (!is.null(x)) {
-    x <- as.data.frame(x)
-    if(dim(x)[2] != 22) stop("La matrice doit les 22 comorbidites DANS L'ORDRE.")
-    if(any(apply(x[, -1], 2, function(x) {length(table(x))}) > 2)) stop("Au moins une des comorbidites possede plus de 2 niveauxs.")
-  } else {
-    x <- cbind(genre
-               age,
-               isch_heart,
-               cerebro,
-               heart_fail,
-               periph_vasc,
-               diabetes,
-               cancer,
-               cancer_hist,
-               schiz,
-               depression
-               substance_abuse,
-               dementia,
-               parkinson,
-               sclerosis,
-               epilepsy,
-               chirnic_resp,
-               infl_bowel,
-               arthritis,
-               AIDS,
-               end_renal,
-               liver_pancreas)
-    if(any(apply(x[, -1], 2, function(x) {length(table(x))}) > 2)) stop("Au moins une des comorbidites possede plus de 2 niveauxs.")
-  }
-  
-  
-  x[, 2] <- floor((x[, 2]-65)/5)
-  x[, 2] <- ifelse(x[, 2] < 0, 0, ifelse(x[, 2] > 7, 7, x[, 2]))
-  MRMI <- x[, 2] +
-    0 * rowSums(x[, c(3, 9, 18, 20)]) +
-    1 * rowSums(x[, c(1, 4, 5, 6, 7, 10, 11, 14, 16, 17, 19)]) + 
-    2 * rowSums(x[, c(12, 13, 15, 21, 22)]) + 
-    3 * x[, 8]
-  
-  ERMI <- x[, 2] +
-    0 * rowSums(x[, c(1, 18)]) +
-    2 * rowSums(x[, c(3, 9, 13)]) + 
-    3 * rowSums(x[, c(4, 5, 6, 16 ,17)]) + 
-    4 * rowSums(x[, c(7, 19)]) + 
-    5 * rowSums(x[, c(11, 12, 14, 22)]) + 
-    6 * x[, 10] +
-    7 * x[, 8] +
-    9 * x[, 15] +
-    10 * x[, 20] +
-    16 * x[, 21]
+MRMI.ERMI <- function(gender = NULL,
+                      age = NULL,
+                      isch.heart = NULL,
+                      cerebro.vasc = NULL,
+                      heart.fail = NULL,
+                      periph.vasc = NULL,
+                      diabetes = NULL,
+                      cancer = NULL,
+                      cancer.hist = NULL,
+                      schizo = NULL,
+                      depression = NULL,
+                      subst.abuse = NULL,
+                      dementia = NULL,
+                      parkinson = NULL,
+                      mult.sclero = NULL,
+                      epilespy = NULL,
+                      chornic.resp = NULL,
+                      infl.bowel = NULL,
+                      rheum.arthr = NULL,
+                      HIV = NULL,
+                      es.renal = NULL,
+                      liver.pancr = NULL) {
 
-  return(list(MRMI = MRMI,
-              ERMI = ERMI))
+  gender <- match.arg(gender, choices = c("M", "F"))
+
+  if (!is.numeric(age)) stop("Age must be numeric (years)")
+
+  if (is.null(gender) | is.null(age) | is.null(isch.heart) | is.null(cerebro.vasc) |
+      is.null(heart.fail) | is.null(periph.vasc) | is.null(diabetes) | is.null(cancer) |
+      is.null(cancer.hist) | is.null(schizo) | is.null(depression) | is.null(subst.abuse) |
+      is.null(dementia) | is.null(parkinson) | is.null(mult.sclero) | is.null(epilespy) |
+      is.null(chornic.resp) | is.null(infl.bowel) | is.null(rheum.arthr) | is.null(HIV) |
+      is.null(es.renal) | is.null(liver.pancr)) {
+    stop("All arguments are required")
+  }
+
+  if (!(all(isch.heart %in% c(0, 1, TRUE, FALSE)) & all(cerebro.vasc %in% c(0, 1, TRUE, FALSE)) &
+      all(heart.fail %in% c(0, 1, TRUE, FALSE)) & all(periph.vasc %in% c(0, 1, TRUE, FALSE)) &
+      all(diabetes %in% c(0, 1, TRUE, FALSE)) & all(cancer %in% c(0, 1, TRUE, FALSE)) &
+      all(cancer.hist %in% c(0, 1, TRUE, FALSE)) & all(schizo %in% c(0, 1, TRUE, FALSE)) &
+      all(depression %in% c(0, 1, TRUE, FALSE)) & all(subst.abuse %in% c(0, 1, TRUE, FALSE)) &
+      all(dementia %in% c(0, 1, TRUE, FALSE)) & all(parkinson %in% c(0, 1, TRUE, FALSE)) &
+      all(mult.sclero %in% c(0, 1, TRUE, FALSE)) & all(epilespy %in% c(0, 1, TRUE, FALSE)) &
+      all(chornic.resp %in% c(0, 1, TRUE, FALSE)) & all(infl.bowel %in% c(0, 1, TRUE, FALSE)) &
+      all(rheum.arthr%in% c(0, 1, TRUE, FALSE)) & all(HIV %in% c(0, 1, TRUE, FALSE)) &
+      all(es.renal %in% c(0, 1, TRUE, FALSE)) & all(liver.pancr %in% c(0, 1, TRUE, FALSE)))) {
+    stop("All arguments accept type must be c(0,1) or c(TRUE, FALSE)")
+  }
+
+  ind_mrmi <- ifelse(gender == "M", 1, 0) + floor((age - 65)/5) +
+      isch.heart * 0 + cerebro.vasc * 1 + heart.fail * 1 + periph.vasc * 1 +
+        diabetes * 1 + cancer * 3 + cancer.hist * 0 + schizo * 1 + depression * 1 +
+          subst.abuse * 2 + dementia * 2 + parkinson * 1 + mult.sclero * 2 + epilespy * 1 +
+            chornic.resp * 1 +  infl.bowel * 0 + rheum.arthr * 1 + HIV * 0 + es.renal * 2 +
+              liver.pancr * 2
+
+ ind_ermi <- ifelse(gender == "M", 0, 0) + floor((age - 65)/5) +
+      isch.heart * 2 + cerebro.vasc * 3 + heart.fail * 3 + periph.vasc * 3 +
+      diabetes * 4 + cancer * 7 + cancer.hist * 2 + schizo * 6 + depression * 5 +
+      subst.abuse * 5 + dementia * 2 + parkinson * 5 + mult.sclero * 9 + epilespy * 3 +
+      chornic.resp * 3 +  infl.bowel * 0 + rheum.arthr * 4 + HIV * 10 + es.renal * 16 +
+      liver.pancr * 5
+
+
+  return(list(
+    MRMI = ind_mrmi,
+    ERMI = ind_ermi
+  ))
 }
